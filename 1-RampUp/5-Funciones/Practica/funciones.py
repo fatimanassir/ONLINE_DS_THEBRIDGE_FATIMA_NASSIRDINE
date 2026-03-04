5030f9fe1656c071b3a9dd7ab645a619897c4625
fatimanassir <tifawt66@gmail.com>
2026-03-04 09:16:29 +0100

    ejercicios python

---
+++ 1-RampUp/5-Funciones/Practica/funciones.py
@@ -0,0 +1,11 @@
+#todas las funciones en una única celda:
+
+def area_cuadrado(base, altura):
+    return  base * altura
+
+def area_triangulo (base, altura):
+    return (base * altura) / 2
+
+import math
+def area_circulo(radio):
+    return math.pi * radio ** 2
